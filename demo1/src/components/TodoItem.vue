3b8e03a7c2e8c91da959b56f87167b816132e2d9
WangShuang <wgpython@163.com>
2019-03-26 16:53:18 +0800

    todo list父子组件实现

---
+++ demo1/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li @click="del_item(index)">
+        <div style="box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)">
+            {{ item }} -- {{ index }}
+        </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'TodoItem',
+        props: ["item", "index"],   // props接收父组件的信息
+        data() {
+            return {}
+        }, methods: {
+            del_item(index){
+                // delete 父组件需要监听
+                this.$emit("delete", index)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo1/src/views/ListTwo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        输入:
+        <el-input placeholder="请输入" v-model="info" style="width:50%"></el-input>
+        <el-button type="primary" @click="add_item(info)">添加</el-button>
+        <p>父子组件之间传递以及$emit回调使用</p>
+        <ul>
+            <todo-item v-for="(info, index) in list_info" :key="index"
+                       :item="info"
+                       :index="index"
+                       @delete="handleDelete"
+
+            ></todo-item>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import TodoItem from '@/components/TodoItem.vue'
+
+    export default {
+        name: 'ListTwo',
+        data() {
+            return {
+                info: '',
+                list_info: [],
+            }
+        },
+        components: {
+            "todo-item": TodoItem,
+        },
+        methods: {
+            add_item(item) {
+                this.list_info.push(item);
+                this.info = ''
+            },
+            handleDelete(index){
+                // 接受子组件过来的参数
+                this.list_info.splice(index, 1)
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+    ul, li {
+        padding: 0;
+        margin: 0;
+        list-style: none
+    }
+</style>
